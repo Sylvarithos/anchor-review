e7187b0cc32f5a360eca2759e9b79bc6cb4b3f0c
devtopstar0201 <mint.skidev0201@gmail.com>
2024-06-03 11:07:17 -0600

    delete complete

--- programs/anchor-movie-review-program/src/lib.rs
+++ programs/anchor-movie-review-program/src/lib.rs
@@ -24,15 +24,31 @@ pub mod anchor_movie_review_program {
         movie_review.description = description;
         Ok(())
     }
+
+    pub fn update_movie_review(
+        ctx: Context<UpdateMovieReview>,
+        title: String,
+        description: String,
+        rating: u8,
+    ) -> Result<()> {
+        msg!("Movie review account space reallocated");
+        msg!("Title: {}", title);
+        msg!("Description: {}", description);
+        msg!("Rating: {}", rating);
+
+        let movie_review = &mut ctx.accounts.movie_review;
+        movie_review.rating = rating;
+        movie_review.description = description;
+
+        Ok(())
+    }
+
+    pub fn delete_movie_review(_ctx: Context<DeleteMovieReview>, title: String) -> Result<()> {
+        msg!("Movie review for {} deleted", title);
+        Ok(())
+    }
 }
 
-#[account]
-pub struct MovieAccountState {
-    pub reviewer: Pubkey,    // 32
-    pub rating: u8,          // 1
-    pub title: String,       // 4 + len()
-    pub description: String, // 4 + len()
-}
 
 #[derive(Accounts)]
 #[instruction(title:String, description:String)]
@@ -48,4 +64,44 @@ pub struct AddMovieReview<'info> {
     #[account(mut)]
     pub initializer: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title:String, description:String)]
+pub struct UpdateMovieReview<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), initializer.key().as_ref()],
+        bump,
+        realloc = 8 + 32 + 1 + 4 + title.len() + 4 + description.len(),
+        realloc::payer = initializer,
+        realloc::zero = true,
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct DeleteMovieReview<'info> {
+    #[account(
+        mut,
+        seeds=[title.as_bytes(), initializer.key().as_ref()],
+        bump,
+        close=initializer
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[account]
+pub struct MovieAccountState {
+    pub reviewer: Pubkey,    // 32
+    pub rating: u8,          // 1
+    pub title: String,       // 4 + len()
+    pub description: String, // 4 + len()
 }
